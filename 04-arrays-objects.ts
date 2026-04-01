261097bfc8f388d6ba3b1252fbaa45582fd86355
elgeeee <ellowlayn@gmail.com>
2026-04-01 16:46:27 +0800

    Done playing and updating the 04-arrays-objects.ts file

--- 04-arrays-objects.ts
+++ 04-arrays-objects.ts
@@ -1,13 +1,13 @@
-let hobbies = ["Eating", "Watching"]
+let hobbies: string[] = ["Dancing", "Reading"];
 
-// hobbies.push(10)
+// hobbies.push(8)
 
-// let userList: (string | number)[];
-let userList: Array<string | number>;
+let userList: (string | number)[];
+let userList2: Array<string | number>;
 
-userList = ["Elmer", "Elmer123", 123456]
+userList = ["Lorlyn Grace", "Lorlyn248", 12468];
 
-// console.log(userList)
+console.log(userList);
 
 let user: {
     name: string;
@@ -18,9 +18,9 @@ let user: {
         id: number
     }
 } = {
-    name: "Elmer",
-    age: 31,
-    hobbies: ["Eating", "Sleeping"],
+    name: "Lorlyn Grace",
+    age: 20,
+    hobbies: ["Dancing", "Reading"],
     role: {
         description: 'admin',
         id: 1
@@ -28,13 +28,13 @@ let user: {
 }
 
 // Must not null or undefined
-let val: {} = "Elmer Alvarado"
+let val: string = "Lorlyn Grace Boiser"
 
 // Flexible Object - Record Type
 let data: Record<string, number | string>
 
 data = {
     1: 1,
-    name: 'Elmer'
+    name: 'Lorlyn Grace'
     // isPartTimeInstructor: true
 }
