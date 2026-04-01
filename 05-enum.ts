399ee8d8abc792a3901631955d372038177a76e0
elgeeee <ellowlayn@gmail.com>
2026-04-01 16:57:59 +0800

    Done playing and updating the 05-enum.ts file

--- 05-enum.ts
+++ 05-enum.ts
@@ -4,11 +4,13 @@
         // SuperAdmin = 1,
         SuperAdmin = 'SuperAdmin',
         // Teacher,
-        Teacher = 'Teacher',
+        Instructor = 'Instructor',
         // Student,
         Student = 'Student',
     }
 
     // let userRole: Role = 0;
     let userRole: Role = Role.SuperAdmin;
+
+    console.log(userRole);
 })
