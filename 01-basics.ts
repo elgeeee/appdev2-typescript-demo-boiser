71953fb8c1326155710de6beb758ba33088f73c0
elgeeee <ellowlayn@gmail.com>
2026-04-01 16:19:34 +0800

    Done playing and updating the 01-basic.ts file

--- 01-basics.ts
+++ 01-basics.ts
@@ -1,20 +1,16 @@
-// console.log("Hello World!")
+console.log("Hello World!")
 
-let userName: string = "Elmer";
-let userAge = 31;
+let userName: string = "Lorlyn Grace";
+let userAge: number = 20;
 
-// ...
-
-userAge = "31"
+userAge = 20
 
 console.log(`Username - ${userName}`)
 console.log(`Age - ${userAge}`)
 
-function add(a: number, b = 5) {
+function add(a: number, b: number = 5): number {
     return a + b
 }
 
-add(10)
-// add('10')
-add(10, 6)
-// add(10, '6')
+console.log(add(10))
+console.log(add(10, 6))
